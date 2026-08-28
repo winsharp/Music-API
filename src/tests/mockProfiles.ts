3be66422b0e8516112a72e67e70fabb2ea40131a
betohhh707 <betohhh69@yahoo.com>
2026-08-27 18:09:07 -0700

    feat: add real Discogs profiles to ratings mock via fetch script

--- src/tests/mockProfiles.ts
+++ src/tests/mockProfiles.ts
@@ -45,23 +45,120 @@ export const mockProfiles: MockProfile[] = [
         ],
     },
     {
-  username: "adiones",
-  ratedReleases: [
-    {
-      id: 26132231,
-      title: "Dance Devil Dance",
-      artist: "Avatar (13)",
-      rating: 5,
-      thumb: "https://i.discogs.com/Laisdplv6YNcGXEhfnRWtpnDnRo2B7KIfp1kJQMJEr8/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI2MTMy/MjMxLTE3NTgwMDU2/MTctNjI5OS5qcGVn.jpeg"
+        username: "adiones",
+        avatarUrl: "https://gravatar.com/avatar/2aa2c1bc348b25db157cd9bef73dccc31b1f882b87e83cbc77afa825de373e7d?s=500&r=pg&d=mm",
+        ratedReleases: [
+            {
+                id: 26132231,
+                title: "Dance Devil Dance",
+                artist: "Avatar (13)",
+                rating: 5,
+                thumb: "https://i.discogs.com/Laisdplv6YNcGXEhfnRWtpnDnRo2B7KIfp1kJQMJEr8/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI2MTMy/MjMxLTE3NTgwMDU2/MTctNjI5OS5qcGVn.jpeg",
+            },
+            {
+                id: 11126459,
+                title: "Master Of Puppets",
+                artist: "Metallica",
+                rating: 5,
+                thumb: "https://i.discogs.com/ePt-b6o7rHA5xBNwx16pN4s5VgdXKhEMZLzIMAmmEgM/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExMTI2/NDU5LTE1Mjk4OTcx/MDYtMzg4MS5qcGVn.jpeg",
+            },
+            {
+                id: 37268751,
+                title: "No Place Of Warmth",
+                artist: "Frozen Soul",
+                rating: 0,
+                thumb: "https://i.discogs.com/dUWxqDFPvt5iRQQbzt1GCMXYlN8Tair6b3e-TchxbW8/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM3MjY4/NzUxLTE3NzgyNjMx/MDgtMzE0OC5qcGVn.jpeg",
+            },
+            {
+                id: 8454111,
+                title: "Forward In Reverse",
+                artist: "Dizzy Mizz Lizzy",
+                rating: 4,
+                thumb: "https://i.discogs.com/peZZew3qFHOMeWYoWvTOzCvE7BfhLHan0XJxVRlrdwI/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg0NTQx/MTEtMTcxMDM1Mjcz/Mi05Mzc4LmpwZWc.jpeg",
+            },
+            {
+                id: 19598281,
+                title: "Radio Free Europe",
+                artist: "R.E.M.",
+                rating: 4,
+                thumb: "https://i.discogs.com/_VKH9UliWelDio4p1ACRWf5vmhIJc74FELqOz2OIoMw/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE5NTk4/MjgxLTE2Mjc2MDMz/MDQtMjk2MS5qcGVn.jpeg",
+            },
+            {
+                id: 38132055,
+                title: "One Assassination Under God (Chapter 2)",
+                artist: "Marilyn Manson",
+                rating: 0,
+                thumb: "https://i.discogs.com/WDJU9Il61NGwkuOv1b3K4ZSxZf1nylKOyatYsnYPcP8/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM4MTMy/MDU1LTE3ODY1ODIy/NjAtMjYwNS5wbmc.jpeg",
+            },
+            {
+                id: 38182971,
+                title: "Alien Metal",
+                artist: "King Gizzard And The Lizard Wizard",
+                rating: 0,
+                thumb: "https://i.discogs.com/meDe2gdep5Aw2oFb1HM3F6wf7W-g4hbM5_ECzulI7To/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM4MTgy/OTcxLTE3ODcwNjIz/NTUtNjI4NC5qcGVn.jpeg",
+            },
+            {
+                id: 38078379,
+                title: "X",
+                artist: "Kylie Minogue",
+                rating: 5,
+                thumb: "https://i.discogs.com/oGxSC9bJb0SlxuWg155xDSCateWpOy702h2gc3B1dQ4/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM4MDc4/Mzc5LTE3ODYyMDI5/OTItMzM1Ni5qcGVn.jpeg",
+            },
+            {
+                id: 10936459,
+                title: "Nomad",
+                artist: "Kublai Khan (4)",
+                rating: 5,
+                thumb: "https://i.discogs.com/tXaYAynKAF3hQmAC183vdrMpL9gLYLdFMwkaZMMHShQ/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwOTM2/NDU5LTE1NzAzNDk3/MTYtMjg1NS5qcGVn.jpeg",
+            },
+            {
+                id: 35347651,
+                title: "K.I.D.S. (Kickin Incredibly Dope Shit)",
+                artist: "Mac Miller",
+                rating: 5,
+                thumb: "https://i.discogs.com/qItVEfxEeWq8rsX6RJA9mDko6VBL2sLgSW0f6zHSl28/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM1MzQ3/NjUxLTE3NjAyMTQ3/MzItMTgzMS5qcGVn.jpeg",
+            },
+        ],
     },
     {
-      id: 11126459,
-      title: "Master Of Puppets",
-      artist: "Metallica",
-      rating: 5,
-      thumb: "https://i.discogs.com/ePt-b6o7rHA5xBNwx16pN4s5VgdXKhEMZLzIMAmmEgM/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTExMTI2/NDU5LTE1Mjk4OTcx/MDYtMzg4MS5qcGVn.jpeg"
+        "username": "betohhh.ha",
+        "avatarUrl": "https://gravatar.com/avatar/3a4cebd0cbac6ec5c073bd01b5926d261c034966419d6bfa1e803c7c8824d56c?s=500&r=pg&d=mm",
+        "ratedReleases": [
+            {
+                "id": 2903217,
+                "title": "Don't Sit Down 'Cause I've Moved Your Chair",
+                "artist": "Arctic Monkeys",
+                "rating": 0,
+                "thumb": "https://i.discogs.com/NPd_sTJRgL4_U-uivgUOqk9x0TRk27IhmiMKooyPj2o/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI5MDMy/MTctMTMwNjg0NzEz/My5qcGVn.jpeg"
+            },
+            {
+                "id": 15730874,
+                "title": "Bald! / Bald! Remix",
+                "artist": "JPEGMAFIA",
+                "rating": 0,
+                "thumb": "https://i.discogs.com/1LLM-HmBbv0PvmRPOuVams3J03dGJYkdFOCzH5iBzug/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE1NzMw/ODc0LTE1OTgxMjg4/NDYtMzIwNi5qcGVn.jpeg"
+            },
+            {
+                "id": 23605793,
+                "title": "Greatest Hits - Revisited - Remixed - Remastered - MMXXII",
+                "artist": "Whitesnake",
+                "rating": 0,
+                "thumb": "https://i.discogs.com/UPccEZf-qiprND-BEKsdT2f7SJJOUbxSt4w_dKTIDg8/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIzNjA1/NzkzLTE2NTYzNjg3/MDktNTAxOC5qcGVn.jpeg"
+            },
+            {
+                "id": 8098759,
+                "title": "Nirvana",
+                "artist": "Nirvana",
+                "rating": 0,
+                "thumb": "https://i.discogs.com/QyJhv99gA6Q1r8MksKKwDyJJV_n35vKC9XPsKOOR-so/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTgwOTg3/NTktMTc1NjUyODAy/MC03MDUyLmpwZWc.jpeg"
+            },
+            {
+                "id": 6400703,
+                "title": "Content Nausea",
+                "artist": "Parquet Courts",
+                "rating": 0,
+                "thumb": "https://i.discogs.com/hkhpydNSePC0WqJQiOqdavq3CRJe8Q0DUD8PW0AHGkE/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY0MDA3/MDMtMTQ1NzExMzU2/Mi05OTgyLmpwZWc.jpeg"
+            }
+        ]
     }
-  ]
-}
-
 ];
